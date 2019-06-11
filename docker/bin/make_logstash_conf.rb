d82b54d687f622e79f512ff94a497444419e6791
Ben Berry <ben.berry@gsa.gov>
2019-06-11 15:23:03 -0700

    Add support for ELB logs
    Create a separate filter path for ELB logs
    Add prefix for different environments

--- docker/bin/make_logstash_conf.rb
+++ docker/bin/make_logstash_conf.rb
@@ -64,64 +64,123 @@ input {
 
 filter_string = %q/
 filter {
+    # we have alb and elb logs in this bucket. Try ALB log format first, then ELB if that fails
     dissect {
         mapping => {
            "message" => '%{[@alb][type]} %{[@alb][timestamp]} %{[@alb][alb][id]} %{[@alb][client][ip]}:%{[@alb][client][port]} %{alb_target_ip_port} %{[@alb][request][processing_time]} %{alb_target_processing_time} %{[@alb][response][processing_time]} %{[@alb][alb][status_code]} %{alb_target_status_code} %{[@alb][received_bytes]} %{[@alb][sent_bytes]} "%{[@alb][request][request]}" "%{[@alb][user_agent]}" %{alb_ssl_cipher} %{alb_ssl_protocol} %{alb_target_group_arn} "%{[@alb][trace_id]}" "%{[@alb][domain_name]}" "%{[@alb][chosen_cert_arn]}" %{[@alb][matched_rule_priority]} %{[@alb][request][creation_time]} "%{[@alb][actions_executed]}" "%{alb_redirect_url}" "%{alb_error_reason}"'
         }
+        id => "alb-dissect"
     }
 
-    prune {
-        # aws uses - to indicate something like null. Removing it lets us safely convert numbers later
-        blacklist_values => [
-                             "alb_error_reason", "^-$",
-                             "alb_target_ip_port", "^-$",
-                             "alb_target_processing_time", "^-$",
-                             "alb_target_status_code", "^-$",
-                             "alb_target_group_arn", "^-$",
-                             "alb_redirect_url", "^-$",
-                             "alb_ssl_cipher", "^-$",
-                             "alb_ssl_protocol", "^-$"
-                            ]
-
-    
-    }
-    mutate {
-        rename => {
-                    "alb_error_reason" => "[@alb][error][reason]"
-                    "alb_target_ip_port" => "[@alb][target][ip_port]"
-                    "alb_target_processing_time" => "[@alb][target][processing_time]"
-                    "alb_target_status_code" => "[@alb][target][status_code]"
-                    "alb_target_group_arn" => "[@alb][target_group_arn]"
-                    "alb_redirect_url" => "[@alb][redirect_url]"
-                    "alb_ssl_cipher" => "[@alb][ssl_cipher]"
-                    "alb_ssl_protocol" => "[@alb][ssl_protocol]"
+    # dissect above will automatically add this tag
+    if "_dissectfailure" not in [tags] {
+        prune {
+            # aws uses - to indicate something like null. Removing it lets us safely convert numbers later
+            blacklist_values => [
+                                 "alb_error_reason", "^-$",
+                                 "alb_target_ip_port", "^-$",
+                                 "alb_target_processing_time", "^-$",
+                                 "alb_target_status_code", "^-$",
+                                 "alb_target_group_arn", "^-$",
+                                 "alb_redirect_url", "^-$",
+                                 "alb_ssl_cipher", "^-$",
+                                 "alb_ssl_protocol", "^-$"
+                                ]
         }
-    }
-    dissect {
-        # this field may be "-", so we can't split it above
-        # but we definitely want it to match client.ip and client.port if it exists
-        mapping => {
-            "[@alb][target][ip_port]" => "%{[@alb][target][ip]}:%{[@alb][target][port]}"
-        }
-        remove_field => [ "[@alb][target][ip_port]" ]
-    }
 
-    mutate {
-        convert => {
-            "[@alb][target][status_code]" => "integer"
-            "[@alb][target][port]" => "integer"
-            "[@alb][client][port]" => "integer"
-            "[@alb][request][processing_time]" => "float"
-            "[@alb][sent_bytes]" => "integer"
-            "[@alb][received_bytes]" => "integer"
-            "[@alb][alb][status_code]" => "integer"
+        mutate {
+            rename => {
+                        "alb_error_reason" => "[@alb][error][reason]"
+                        "alb_target_ip_port" => "[@alb][target][ip_port]"
+                        "alb_target_processing_time" => "[@alb][target][processing_time]"
+                        "alb_target_status_code" => "[@alb][target][status_code]"
+                        "alb_target_group_arn" => "[@alb][target_group_arn]"
+                        "alb_redirect_url" => "[@alb][redirect_url]"
+                        "alb_ssl_cipher" => "[@alb][ssl_cipher]"
+                        "alb_ssl_protocol" => "[@alb][ssl_protocol]"
+            }
         }
-    }
 
-    date {
-        match => [ "[@alb][timestamp]", "ISO8601"]
+        dissect {
+            # this field may be "-", so we can't split it above
+            # but we definitely want it to match client.ip and client.port if it exists
+            mapping => {
+                "[@alb][target][ip_port]" => "%{[@alb][target][ip]}:%{[@alb][target][port]}"
+            }
+            remove_field => [ "[@alb][target][ip_port]" ]
+        }
+
+        mutate {
+            convert => {
+                "[@alb][target][status_code]" => "integer"
+                "[@alb][target][port]" => "integer"
+                "[@alb][client][port]" => "integer"
+                "[@alb][request][processing_time]" => "float"
+                "[@alb][sent_bytes]" => "integer"
+                "[@alb][received_bytes]" => "integer"
+                "[@alb][alb][status_code]" => "integer"
+            }
+        }
+
+        date {
+            match => [ "[@alb][timestamp]", "ISO8601"]
+        }
+    } else {
+        dissect {
+            mapping => {
+                "message" => '%{[@elb][timestamp]} %{[@elb][elb]} %{[@elb][client][ip]}:%{[@elb][client][port]} %{[elb_target_ip_port]} %{[@elb][request][processing_time]} %{[elb_target_processing_time]} %{[@elb][response][processing_time]} %{[@elb][elb][status_code]} %{[elb_target_status_code]} %{[@elb][received_bytes]} %{[@elb][sent_bytes]} "%{[@elb][request][verb]}" %{[@elb][request][url]} %{[@elb][request][protocol]}" "%{[@elb][request][user_agent]}" %{[@elb][ssl][cipher]} %{[@elb][ssl][protocol]}'
+            }
+            remove_tag => [ "_dissectfailure" ]
+            id => "elb-dissect"
+        }
+        prune {
+            # aws uses - to indicate something like null. Removing it lets us safely convert numbers later
+            blacklist_values => [
+                                 "elb_target_ip_port", "^-$",
+                                 "elb_target_processing_time", "^-$",
+                                 "elb_target_status_code", "^-$",
+                                 "elb_ssl_cipher", "^-$",
+                                 "elb_ssl_protocol", "^-$"
+                                ]
+        }
+
+        mutate {
+            rename => {
+                        "elb_target_ip_port" => "[@elb][target][ip_port]"
+                        "elb_target_processing_time" => "[@elb][target][processing_time]"
+                        "elb_target_status_code" => "[@elb][target][status_code]"
+                        "elb_ssl_cipher" => "[@elb][ssl_cipher]"
+                        "elb_ssl_protocol" => "[@elb][ssl_protocol]"
+            }
+        }
+
+        dissect {
+            # this field may be "-", so we can't split it above
+            # but we definitely want it to match client.ip and client.port if it exists
+            mapping => {
+                "[@elb][target][ip_port]" => "%{[@elb][target][ip]}:%{[@elb][target][port]}"
+            }
+            remove_field => [ "[@elb][target][ip_port]" ]
+        }
+
+        mutate {
+            convert => {
+                "[@elb][target][status_code]" => "integer"
+                "[@elb][target][port]" => "integer"
+                "[@elb][client][port]" => "integer"
+                "[@elb][request][processing_time]" => "float"
+                "[@elb][sent_bytes]" => "integer"
+                "[@elb][received_bytes]" => "integer"
+                "[@elb][elb][status_code]" => "integer"
+            }
+        }
+
+        date {
+            match => [ "[@elb][timestamp]", "ISO8601"]
+        }
     }
 }
+
 /
 
 output_template = %q/
